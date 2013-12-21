LineageOS/android_frameworks_opt_telephony

Telephony: Add getLteOnGsmMode() method (2/2)
Same functionality as the existing getLteOnCdmaMode, but for GSM
LTE devices.
Enable with the telephony.lteOnGsmDevice system property

From CM 10.0: Ibfb47ca608e51393b99d3308e0a6c66050b3f32e
- Moved getLteOnGsmModeStatic() to TelephonyManager.

src/java/com/android/internal/telephony/PhoneBase.java
@@ -1192,6 +1192,14 @@ public abstract class PhoneBase extends Handler implements Phone {
     }
 
     /**
+     * {@hide}
+     */
+    @Override
+    public int getLteOnGsmMode() {
+        return mCM.getLteOnGsmMode();
+    }
+
+    /**
      * Sets the SIM voice message waiting indicator records.
      * @param line GSM Subscriber Profile Number, one-based. Only '1' is supported
      * @param countWaiting The number of messages waiting, if known. Use

IllusionRom-deprecated/android_platform_frameworks_opt_telephony

Change-Id: I9144ef279ca9c8cb9e657a28342f0f4cb3e7deb4

src/java/com/android/internal/telephony/PhoneBase.java
@@ -1383,6 +1383,14 @@ public abstract class PhoneBase extends Handler implements Phone {
     }
 
     /**
+     * {@hide}
+     */
+    @Override
+    public int getLteOnGsmMode() {
+        return mCi.getLteOnGsmMode();
+    }
+
+    /**
      * Sets the SIM voice message waiting indicator records.
      * @param line GSM Subscriber Profile Number, one-based. Only '1' is supported
      * @param countWaiting The number of messages waiting, if known. Use